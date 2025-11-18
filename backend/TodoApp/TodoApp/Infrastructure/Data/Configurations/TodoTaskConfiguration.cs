32bb374ee105121c5a6b5492dbbc51fbf1a03a21
JuliaGrze <julia.grzesiewicz@op.pl>
2025-11-18 17:46:35 +0100

    feat(domain): add TodoTask entity with BaseEntity and EF Core configuration

---
+++ backend/TodoApp/TodoApp/Infrastructure/Data/Configurations/TodoTaskConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TodoApp.Domain.Entities;
+
+namespace TodoApp.Infrastructure.Data.Configurations
+{
+    public class TodoTaskConfiguration : IEntityTypeConfiguration<TodoTask>
+    {
+        public void Configure(EntityTypeBuilder<TodoTask> builder)
+        {
+            builder.ToTable("todo_tasks");
+
+            builder.Property(t => t.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(t => t.Description)
+                .HasMaxLength(1000);
+
+            builder.Property(t => t.IsDone)
+                .IsRequired();
+        }
+    }
+}
---
+++ backend/TodoApp/TodoApp/Infrastructure/Data/TodoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Domain.Entities;
+using TodoApp.Infrastructure.Data.Configurations;
+
+namespace TodoApp.Infrastructure.Data
+{
+    public class TodoDbContext : DbContext
+    {
+        public DbSet<TodoTask> TodoTasks => Set<TodoTask>();
+
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TodoTaskConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
